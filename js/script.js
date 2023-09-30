da0e7450b5e9cce4f12a1f2087850dd1ab569e98
LordRider38 <jmf11.cordoba@gmail.com>
2023-09-29 21:06:08 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Menu hamburguesa
+function mostrar(){
+    document.getElementById("sliding_menu") .classList.add("open")
+}
+
+function ocultar(){
+    document.getElementById("sliding_menu") .classList.remove("open")
+}
+
