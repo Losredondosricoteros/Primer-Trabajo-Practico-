c38b93c28a37cf3304a758b2a7a5142110e74c58
LordRider38 <jmf11.cordoba@gmail.com>
2023-10-21 21:03:06 -0300

    casi lista

--- js/scripts.js
+++ js/scripts.js
@@ -8,29 +8,92 @@ function ocultar(){
 }
 
 
-//Validar Formulario
-let nombre =document.getElementById('nombre');
-let email =document.getElementById('email');
-let pass =document.getElementById('password');
-let error =document.getElementById('error');
-error.style.color = 'red';
 
-function enviarFormulario(){
-    console.log('Enviando formulario...');
+//go top
+ window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+      if(document.documentElement.scrollTop > 600){
+       document.querySelector('.go-top-container')
+        .classList.add('show');
+      }else {
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+     }
+    }
+        document.querySelector('.go-top-btn')
+        .addEventListener('click',() =>{
+           window.scrollTo({
+           top:0,
+          behavior: 'smooth',
+     });
+  });
+  
+  function validarFormulario() {
+    // Obtener los valores ingresados por el usuario y recortar
+    // los posibles espacios en blanco al principio y al final.
+    let usuario = document.getElementById("usuario");
+    let pass = document.getElementById("pass");
+    let error = false;
+    document.getElementById("validar_usuario").innerHTML="";
+    document.getElementById("validar_pass").innerHTML="";
+    // Verificar si algún campo está en blanco
+    if (usuario.value == ""  ) {
+        document.getElementById("validar_usuario").innerHTML="Debe completar este campo";
+        error = true;
+        usuario.focus();
+    }
 
-    let mensajesError = [];
-    if(nombre.value === null  || nombre.value === '' ){
-        mensajesError.push('Ingresa tu nombre');
-    }
-    if(email.value === null  || email.value === '' ){
-        mensajesError.push('Ingresa tu Email');
-    }
-    if(pass.value === null  || pass.value === '' ){
-        mensajesError.push('Ingresa tu password');
-    }
-     error.innerHTML = mensajesError.join(' , ');
-    return false;
+    if (pass.value.length<8) {
+      document.getElementById("validar_pass").innerHTML = "Debe completar el clave con 8 carateres como minimo";
+      error = true;
+      pass.focus();
+      error = true
+  }
+
+  if (error == false) {
+      alert("Dato enviado")
+      document.getElementById("usuario").value = ""
+      document.getElementById("validar_usuario").innerHTML = "";
+      document.getElementById("pass").value = ""
+      document.getElementById("validar_pass").innerHTML = "";
+  }
+
+
+
+
+  if (error)
+      return false;
+  else
+      return true;
 }
+  
 
+const { createApp } = Vue
+createApp({
+    data() {
+       return {
+           url:'./js/datos.json',
+           error: false,
+           datos: [],
+          
+      
+    }
+   },
+    methods: {
+      fetchData(url) {
+        fetch(url  )
+            .then(response => response.json())
+            .then(  data => {
+                console.log(data)
+                this.datos=data.empleados
 
+        
+        });
+    }
+    },
+    created() {
+      this.fetchData(this.url)
+    
+   },
+}).mount('#app')
 
